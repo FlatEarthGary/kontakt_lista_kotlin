1c859f022cfc14cee4fc071532c646a42e85bf0b
Fiskett <oliver@fehre.com>
2023-02-15 10:57:50 +0100

    början på contactLIst klasas

--- src/main/kotlin/contactList.kt
+++ src/main/kotlin/contactList.kt
@@ -1,6 +1,7 @@
 class contactList {
     var contacts = mutableListOf<Contact>()
-    fun addContact(first_name: String, ) {
+    fun addContact(first_name: String,last_name: String, phone_number: String, mail: String) {
+       contacts.add(Contact(first_name, last_name, phone_number, mail));
 
     }
 
