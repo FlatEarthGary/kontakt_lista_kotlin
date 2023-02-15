4a85ed328dc6dc47d63789808811546bbe417bfd
Jeff <Jonatanvarfeldt@gmail.com>
2023-02-15 10:35:44 +0100

    saveContactsToFile class implementation

--- src/main/kotlin/contactList.kt
+++ src/main/kotlin/contactList.kt
@@ -1,7 +1,17 @@
+import java.io.File
+
 class contactList {
     var contacts = mutableListOf<Contact>()
     fun addContact(first_name: String, ) {
 
+    }
+
+    fun saveContactsToFile() {
+        val fileObj = File("contacts_database.txt");
+        for (contact: Contact in contacts) {
+            fileObj.writeText("${contact.first_name},${contact.last_name}, ${contact.mail_address}, ${contact.phone_number}\n");
+        }
+        println("Successfully written to database");
     }
 
 }
