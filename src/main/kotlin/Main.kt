e8c5fe1216a7d486d0111cfb50a8af664fbd3e46
Jeff <Jonatanvarfeldt@gmail.com>
2023-03-03 17:18:19 +0100

    saveContactsToFile class implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,14 @@
 fun main() {
     val book = contactList();
+
+    /*
+    book.addContact("Jonte", "Yee", "123321123", "yee@gmail.com");
+    book.addContact("Sven", "Pung", "321321321", "pung@gmail.com");
+    book.addContact("Stefan", "Feg", "222222", "steffe@yahoo.com");
+     */
+
     book.readContactsFromFile();
 
     book.printAll();
-
 
 }
--- src/main/kotlin/contactList.kt
+++ src/main/kotlin/contactList.kt
@@ -1,4 +1,8 @@
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.ObjectInputStream
+import java.io.ObjectOutputStream
 
 class contactList {
     var contacts = mutableListOf<Contact>();
@@ -8,41 +12,44 @@ class contactList {
     }
 
     fun saveContactsToFile() {
+        /*
+        val fileObj: FileOutputStream = FileOutputStream("contacts_database.bin"); // Ändra till filename
+        val stream = ObjectOutputStream(fileObj);
+
+        val test = Contact("a", "a", "a", "a");
+         */
+
         val fileObj = File(filename);
         for (contact: Contact in contacts) {
-            fileObj.appendText("${contact.first_name},${contact.last_name},${contact.mail_address},${contact.phone_number}\n");
+            fileObj.appendText("${contact.first_name}\n${contact.last_name}\n${contact.mail_address}\n${contact.phone_number}\n");
         }
         println("Successfully written to database");
+
     }
 
     fun readContactsFromFile() {
+        var i = 0;
         val fileObj = File(filename);
         val fileContentString = fileObj.readText();
 
-        var fileContent = fileContentString.split(',');
+        var fileContent = fileContentString.split('\n');
 
-        val amountOfContacts = fileContentString.count { it == ','} / 3;
+        val amountOfContacts = fileContentString.count { it == '\n'} / 4;
 
-        for (i in 0 until amountOfContacts)
+        for (p in 0 until amountOfContacts)
         {
-            for (i in fileContent)
-            {
-                println(i);
-            }
-            println("----------------------------");
-            readln();
+            contacts.add(Contact(fileContent[i], fileContent[i+1], fileContent[i+2], fileContent[i+3]));
 
-            contacts.add(Contact(fileContent[0], fileContent[1], fileContent[2], fileContent[3]));
-
-            fileContent = fileContent.drop(4);
+            i += 4;
 
         }
+
     }
 
     fun printAll() {
         for (con in contacts)
         {
-            println("Contact: ${con.first_name}");
+            println("Contact: ${con.first_name} | ${con.last_name} | ${con.phone_number} | ${con.mail_address}");
         }
     }
 }
