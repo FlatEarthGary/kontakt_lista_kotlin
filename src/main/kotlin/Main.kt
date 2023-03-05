c9577beacbdcbb5f002851e92345cbf8115f7e56
Jeff <Jonatanvarfeldt@gmail.com>
2023-03-05 15:06:09 +0100

    readContactsFromFile(), saveContactsToFile(), editContact() och printFullContactList() klar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,17 @@
 fun main() {
-    val book = contactList();
+    val book = ContactList();
 
     /*
     book.addContact("Jonte", "Yee", "123321123", "yee@gmail.com");
     book.addContact("Sven", "Pung", "321321321", "pung@gmail.com");
     book.addContact("Stefan", "Feg", "222222", "steffe@yahoo.com");
+
+    book.saveContactsToFile();
      */
 
     book.readContactsFromFile();
 
-    book.printAll();
+    book.editContact("123321123");
+
 
 }
