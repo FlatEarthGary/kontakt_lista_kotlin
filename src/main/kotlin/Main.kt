60d1975b5fa131e94965b40c4de89f4cc9e652ae
Jeff <Jonatanvarfeldt@gmail.com>
2023-03-05 17:18:00 +0100

    Donzo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,16 +3,27 @@ import kotlin.contracts.contract
 fun main() {
     val book = ContactList();
 
+
     book.addContact("Oliver","Fisk","123","oliver@fisk.se");
-    book.saveContactsToFile()
-    book.readContactsFromFile();
+    book.addContact("Jeff", "Jefferson", "+46 432453211", "jeff@jeff.se");
+    book.addContact("Anders", "A", "123321", "anders@gmail.com");
+
+    /*
+    val yee = Contact("robban", "roberson", "12332131221", "robert@robban.se");
+    book.editContact(yee, "anders@gmail.com");
+    */
+
+    //book.removeContact("oliver@fisk.se");
+    book.printFullContactList();
+
+    //book.addContact("Stefan", "Pung", "33333", "stefan@pung.com");
 
     /* FiskTest*/
+
+    /*
     var newcontact = Contact("Oliver","Fisk","546","oliver@fisk.se")
 
     book.editContact(newcontact, "oliver@fisk.se")
-
+     */
     /* No more FiskTest*/
-
-    book.printAll();
 }
--- src/main/kotlin/contactList.kt
+++ src/main/kotlin/contactList.kt
@@ -1,35 +1,45 @@
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.ObjectInputStream
-import java.io.ObjectOutputStream
 
 class ContactList {
+    // lista som innehåller alla kontakter
     private var contacts = mutableListOf<Contact>();
+    // Namn på fil som kontakterna är sparade/skrivs till
     private val filename: String = "contacts_database.txt";
+    // Funktion som lägger till en kontakt i filen
     fun addContact(first_name: String,last_name: String, phone_number: String, mail: String) {
-       contacts.add(Contact(first_name, last_name, phone_number, mail));
+        // Skapar ett File objekt som låter oss skriva till filen med vårt filename
+        // Vi lägger till alla parametrar som funktionen har tagit emot i filen och separerar varje parameter
+        // med en newline character (\n)
+        File(filename).appendText("${first_name}\n${last_name}\n${phone_number}\n${mail}\n");
     }
+    // Funktion som tar bort kontakten med den tillgivna mail adressen
     fun removeContact(mail: String)
     {
-        var tempcontacts = mutableListOf<Contact>();
+        // Skapar en tempoärar lista av kontakter
+        var temp_contacts = mutableListOf<Contact>();
+        // Läser in alla kontakter och sparar dom i contacts listan
         readContactsFromFile();
+        // Itererar över alla kontakter vi nyss läste in
         for (contact: Contact in contacts)
         {
+            // Om mail adressen av kontakten inte stämmer överens med mail adressen vi fick som parameter
             if (contact.mail_address != mail)
             {
-                tempcontacts.add(contact)
+                temp_contacts.add(contact) // så lägger vi till kontakten i vår temporära lista
             }
         }
-        contacts = tempcontacts;
-        saveContactsToFile();
+        contacts = temp_contacts; // När vi har itererat över alla kontakter och hoppat över dom som stämmer
+                                  // med mail adressen så för vi över temp_contacts till contacts
+        saveContactsToFile(); // Sparar vår contacts lista til fil
     }
+    // Funktion som redigerar en kontakt genom att ta emot mail adressen av kontakten
     fun editContact(editedContact: Contact, oldEmailAdress: String)
     {
-        readContactsFromFile();
-        for (contact: Contact in contacts)
+        readContactsFromFile() // Läser in kontakterna från filen
+        for (contact: Contact in contacts) // Itererar över kontakterna
         {
-            if (contact.mail_address == oldEmailAdress)
+            if (contact.mail_address == oldEmailAdress) // Om kontaktens mail adress stämmer överens med den tillgivna
+                                                        // så ändras alla kontaktens fält till den kontakten som gavs till funktionen
             {
                 contact.first_name = editedContact.first_name
                 contact.last_name = editedContact.last_name
@@ -37,81 +47,91 @@ class ContactList {
                 contact.mail_address  = editedContact.mail_address
             }
         }
-        saveContactsToFile();
+        saveContactsToFile(); // Sparar till fil
     }
 
-    fun saveContactsToFile() {
-        /*
-        val fileObj: FileOutputStream = FileOutputStream("contacts_database.bin"); // Ändra till filename
-        val stream = ObjectOutputStream(fileObj);
-
-        val test = Contact("a", "a", "a", "a");
-         */
-
-        val fileObj = File(filename);
-        fileObj.writeText("");
-        for (contact: Contact in contacts) {
+    // Privat funktion som skriver till fil
+    private fun saveContactsToFile() { //
+        val fileObj = File(filename); // Öppnar filen som vi sparar/skriver till
+        fileObj.writeText(""); // Rensar filen
+        for (contact: Contact in contacts) { // Itererar över dom nyss inlästa kontakerna
             fileObj.appendText("${contact.first_name}\n${contact.last_name}\n${contact.phone_number}\n${contact.mail_address}\n");
+            // Lägger till i filen kontaktens fält separerade med newline
         }
-
     }
 
-    fun readContactsFromFile() {
-        var i = 0;
-        val fileObj = File(filename);
-        val fileContentString = fileObj.readText();
-        contacts.clear();
-        var fileContent = fileContentString.split('\n');
+    // Privat funktion som läser från fil och sparar innehållet i vår contacts lista
+    private fun readContactsFromFile() {
+        var attr = 0; // Variabel som håller koll på hur många fält som skrivits av Contact objektet
+        val contact_attr = 4;
+        val fileObj = File(filename); // Öppnar filen
+        val fileContentString = fileObj.readText(); // Läser in allt innehåll
+        contacts.clear(); // Rensar contacts listan
+        var fileContent = fileContentString.split('\n'); // Separerar det inlästa innehållet där varje
+                                                                   // newline separerar ett fält till Contact klassen
 
-        val amountOfContacts = fileContentString.count { it == '\n'} / 4;
+        val amountOfContacts = fileContentString.count { it == '\n'} / contact_attr; // Räknar hur många newline det finns i filen
+                                                                                     // och då vi vet att varje Contact objekt består av
+                                                                                     // fyra stycken fält så får vi antalet kontakter skrivna
+                                                                                     // till filen genom att dividera med fyra
 
-        for (p in 0 until amountOfContacts)
+        for (p in 0 until amountOfContacts) // Itererar över mängden kontakter
         {
-            contacts.add(Contact(fileContent[i], fileContent[i+1], fileContent[i+2], fileContent[i+3]));
+            // Skapar en ny Contact instans och lägger till den till contacts listan.
+            contacts.add(Contact(fileContent[attr], fileContent[attr+1], fileContent[attr+2], fileContent[attr+3]));
+            // attr börjar på 0 vilket ger att vi får första värdet i filen, andra värdet genom attr+1 o.s.v
 
-            i += 4;
+            attr += contact_attr;
+            // Vi har skrivit fyra värden vilket ger att vi nu måste hoppa fyra värden framåt för att inte
+            // skriva samma värden som innan.
 
         }
     }
 
+    // Funktion som redigerar en kontakt genom att ta emot ett telefonnummer
     fun editContact(phone_number: String) {
-        for (contact: Contact in contacts) {
-            if (phone_number == contact.phone_number)
+        readContactsFromFile(); // Läser in alla kontakter från fil
+        for (contact: Contact in contacts) { // Itererar över kontakterna
+            if (phone_number == contact.phone_number) // Om det angivna telefonnumret stämmer med kontaktens telefonnummer
             {
-                println("Vad vill du ändra?\n[1] Förnamn\n[2] Efternamn\n[3] Telefonnummer\n[4] Mail adress\n\n[>] ");
-                val choice = readln();
+                print("Vad vill du ändra?\n[1] Förnamn\n[2] Efternamn\n[3] Telefonnummer\n[4] Mail adress\n\n[>] ");
+                val choice = readln(); // Läser in från användaren
                 if (choice == "1")
                 {
-                    println("Förnamn: ${contact.first_name}\nNytt förnamn: ");
-                    contact.first_name = readln();
+                    print("Förnamn: ${contact.first_name}\nNytt förnamn: "); // Ger det gamla namnet och sedan frågar efter det nya
+                    contact.first_name = readln(); // Läser in det nya värdet och tillger det kontaktens fält
                 }
                 else if (choice == "2") {
-                    println("Efternamn: ${contact.last_name}\nNytt efternamn: ");
+                    print("Efternamn: ${contact.last_name}\nNytt efternamn: ");
                     contact.last_name = readln();
                 }
                 else if (choice == "3") {
-                    println("Telefonnummer: ${contact.phone_number}\nNytt telefonnummer: ");
+                    print("Telefonnummer: ${contact.phone_number}\nNytt telefonnummer: ");
                     contact.phone_number = readln();
                 }
                 else if (choice == "4") {
-                    println("Mail adress: ${contact.mail_address}\nNy mail adress: ");
+                    print("Mail adress: ${contact.mail_address}\nNy mail adress: ");
                     contact.mail_address = readln();
                 }
-                else {
+                else { // Om inget icke valbart alternativ finns
                     println("Icke valbart alternativ!");
                     return;
                 }
-                saveContactsToFile();
+                saveContactsToFile(); // Skriver förändringarna till fil
                 return;
             }
         }
         println("Kunde inte hitta kontakten :(");
     }
 
+    // Funktion som skriver ut hela telefonlistan
     fun printFullContactList() {
-        for (con in contacts)
+        readContactsFromFile(); // Läser in alla kontakter från fil
+        contacts.sortBy { it.first_name }; // Sorterar dom efter förnamn
+        for (con: Contact in contacts) // Itererar över kontakterna
         {
-            println("First name: ${con.first_name} | Last name: ${con.last_name} |Phone number: ${con.phone_number} | Mail address: ${con.mail_address}");
+            // Skriver ut värdena
+            println("First name: ${con.first_name} | Last name: ${con.last_name} | Phone number: ${con.phone_number} | Mail address: ${con.mail_address}");
         }
     }
 }
